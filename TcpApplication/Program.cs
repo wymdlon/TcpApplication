b7cad457009aba9b4b11b7eba1767e170b61d62d
Влад Демченко <vdemchenko99@gmail.com>
2019-11-05 01:02:51 +0300

    Add project files.

---
+++ TcpApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainServer server = new MainServer();
+            Thread listenThread = new Thread(new ThreadStart(server.Listen));
+
+            try
+            {
+                listenThread.Start();
+            }
+            catch (Exception e)
+            {
+                server.Disconnect();
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
