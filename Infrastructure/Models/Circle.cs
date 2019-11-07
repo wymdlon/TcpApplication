d4b0a94ea339ee0e8e0a572bfbff512361a2a250
Vladyslav Demchenko <vladyslav.demchenko.99@gmail.com>
2019-11-08 01:08:43 +0300

    update model for deserialize

--- Infrastructure/Models/Circle.cs
+++ Infrastructure/Models/Circle.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Infrastructure.Models
 {
     public class Circle
     {
         public const int MinXBound = 0;
-        public const int MaxXBound = 50;
+        public const int MaxXBound = 1000;
 
         public const int MinYBound = 0;
-        public const int MaxYBound = 20;
+        public const int MaxYBound = 1000;
 
         public int Radius { get; private set; }
         public int X { get; protected internal set; }
         public int Y { get; protected internal set; }
+
+        [JsonConstructor]
+        public Circle(int radius, int x, int y)
+        {
+            Radius = radius;
+            X = x;
+            Y = y;
+        }
 
         public Circle()
         {
